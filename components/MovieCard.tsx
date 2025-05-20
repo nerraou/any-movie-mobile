b19623f1b502484d64311ca76a67b3315db69c37
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2025-05-20 11:53:57 +0100

    Update MovieCard to show rating and release year

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
-import { Image, Text, TouchableOpacity } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { Link } from "expo-router";
+import { icons } from "@/constants/icons";
 
 function MovieCard({
   id,
@@ -9,7 +10,7 @@ function MovieCard({
   release_date,
 }: Movie) {
   return (
-    <Link href={`/movie/${id}`} asChild>
+    <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
           source={{
@@ -19,7 +20,26 @@ function MovieCard({
           }}
           className="w-full h-52 rounded-lg"
         />
-        <Text className="text-white text-base font-semibold mt-2">{title}</Text>
+        <Text
+          className="text-white text-base font-semibold mt-2"
+          numberOfLines={1}
+        >
+          {title}
+        </Text>
+        <View className="flex flex-row items-center justify-start gap-x-1">
+          <Image source={icons.star} className="size-4" />
+          <Text className="text-xs text-white font-bold">
+            {Math.round(vote_average / 2)}
+          </Text>
+        </View>
+        <View className="flex-row items-center justify-between">
+          <Text className="text-xs text-colors-light-300 font-medium mt-1">
+            {release_date?.split("-")[0]}
+          </Text>
+          {/*<Text className="text-xs text-colors-light-300 font-medium uppercase">*/}
+          {/*  Movie*/}
+          {/*</Text>*/}
+        </View>
       </TouchableOpacity>
     </Link>
   );
