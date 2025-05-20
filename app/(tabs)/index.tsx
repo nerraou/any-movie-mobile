fbcb5babe9c69db4c3025a173409a38a71e67a8c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2025-05-21 00:01:04 +0100

    feat: Add trending movies feature and Appwrite integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,9 +14,17 @@ import { useRouter } from "expo-router";
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
+import TrendingCard from "@/components/TrendingCard";
 
 export default function Index() {
   const route = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -40,15 +48,15 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000fff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
+        ) : moviesError || trendingError ? (
           <Text className="text-lg text-white font-bold mt-5 mb-3">
-            Error: {moviesError.message}
+            Error: {moviesError?.message || trendingError?.message}
           </Text>
         ) : (
           <View className="flex-1 mt-5">
@@ -56,23 +64,46 @@ export default function Index() {
               onPress={() => route.push("/search")}
               placeholder="Search for a movie"
             />
-            <Text className="text-lg text-white font-bold mt-5 mb-3">
-              Latest movies
-            </Text>
-            <FlatList
-              data={movies}
-              renderItem={({ item }) => <MovieCard {...item} />}
-              keyExtractor={(item) => item.id.toString()}
-              numColumns={3}
-              columnWrapperStyle={{
-                justifyContent: "flex-start",
-                gap: 20,
-                paddingRight: 5,
-                marginBottom: 10,
-              }}
-              className="mt-2 pb-32"
-              scrollEnabled={false}
-            />
+            {trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-white font-bold mb-3">
+                  Trending Movies
+                </Text>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  className="mb-4 mt-3"
+                  data={trendingMovies}
+                  contentContainerStyle={{
+                    gap: 26,
+                  }}
+                  renderItem={({ item, index }) => (
+                    <TrendingCard movie={item} index={index} />
+                  )}
+                  keyExtractor={(item) => item.movie_id.toString()}
+                  ItemSeparatorComponent={() => <View className="w-4" />}
+                />
+              </View>
+            )}
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">
+                Latest movies
+              </Text>
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
           </View>
         )}
       </ScrollView>
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -7,6 +7,7 @@ import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import { icons } from "@/constants/icons";
 import SearchBar from "@/components/SearchBar";
+import { updateSearchCount } from "@/services/appwrite";
 
 export default function Search() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -30,6 +31,12 @@ export default function Search() {
 
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
+
+  useEffect(() => {
+    if (movies?.length > 0 && movies[0]) {
+      updateSearchCount(searchQuery, movies[0]);
+    }
+  }, [movies]);
 
   return (
     <View className="flex-1 bg-colors-primary">
